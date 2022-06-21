0efd6f127086ac88a8b55b321c03b76ae2a4ca36
cena <sina.jafari1970@gmail.com>
2022-06-21 23:09:53 +0430

    route edited

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,14 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Test;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    public function index($id){
-        $category = Category::find($id);
-//        dd($category->food);
-        return response()->json($category->food);
-//        dd($category);
+    public function index(Request $request){
+//        foreach ($request->food_id as $item) {
+//            $test= Test::insert([
+//                'order_id' => $request->order_id,
+//                'food_id' => $item
+//            ]);
+//       }
+////       return response()->json($test);
+//        $input =$request->except('_token');
+//        return response($input);
+//        $data = json_decode(json_encode($input),true);
+//        $res = Test::insert($data);
+//        return $res;
+//        foreach ($request->food_id as $item){
+//            $test= Test::insert([
+//                'order_id' => $request->order_id,
+//                'food_id' => $item
+//            ]);
+//        }
+        $arr = array(['order_id'=>1,'food_id'=>40],["order_id"=>1,"food_id"=>60],["order_id"=>1,"food_id"=>70]);
+        $test = Test::insert($arr);
+        return response($test);
     }
 }

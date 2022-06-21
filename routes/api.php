923f057ec173d73ae2233cca6ad7f65ea122c223
cena <sina.jafari1970@gmail.com>
2022-06-21 23:09:28 +0430

    route edited

--- routes/api.php
+++ routes/api.php
@@ -23,26 +23,28 @@ use Illuminate\Support\Facades\Route;
 //CLIENT
 Route::post('client/login',[LoginController::class, 'clientLogin'])->name('clientLogin');
 Route::post('client/register',[LoginController::class, 'clientRegister'])->name('clientRegister');
-Route::post('client/addaddress',[AddressController::class,'addAddress']);
-Route::get('client/showaddress/{client_id}',[AddressController::class,'showClientAddresses']);
-Route::post('addorder',[OrderController::class,'store']);
-Route::apiResource('order',OrderController::class);
+
 
 Route::group( ['prefix' => 'client','middleware' => ['auth:client-api','scopes:client'] ],function(){
     // authenticated staff routes here
     Route::get('dashboard',[LoginController::class, 'clientDashboard']);
+    Route::post('addaddress',[AddressController::class,'addAddress']);
+    Route::get('showaddress/{client_id}',[AddressController::class,'showClientAddresses']);
+    Route::post('addorder',[OrderController::class,'store']);
+    Route::apiResource('order',OrderController::class);
 });
 
 //ADMIN
 //Route::post('user/register',[LoginController::class, 'userRegister'])->name('userRegister');
-Route::apiResource('categories',CategoryController::class);
-Route::apiResource('foods',FoodsController::class);
-Route::get('menu/{category_id}',[MenuController::class,'showMenu']);
-Route::apiResource('orders',AdminOrderController::class);
+
 Route::post('user/login',[LoginController::class, 'userLogin'])->name('userLogin');
 Route::group( ['prefix' => 'user','middleware' => ['auth:user-api','scopes:user'] ],function(){
     // authenticated staff routes here
     Route::get('dashboard',[LoginController::class, 'userDashboard']);
+    Route::apiResource('categories',CategoryController::class);
+    Route::apiResource('foods',FoodsController::class);
+    Route::get('menu/{category_id}',[MenuController::class,'showMenu']);
+    Route::apiResource('orders',AdminOrderController::class);
 });
 
 
